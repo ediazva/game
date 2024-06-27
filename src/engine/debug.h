cbb2d04f340d9ad4f5ebe10ac656266d231ba848
ediazva <ediazva@unsa.edu.pe>
2024-06-26 20:44:07 -0500

    Added DEBUG_WARNING

--- src/engine/debug.h
+++ src/engine/debug.h
@@ -7,6 +7,8 @@ namespace engine {
 #ifdef GAME_DEBUG
 #  include "engine/raylib.h"
 #  define DEBUG_TRACE(...) raylib::TraceLog(raylib::LOG_TRACE, __VA_ARGS__)
+#  define DEBUG_WARNING(...) raylib::TraceLog(raylib::LOG_WARNING, __VA_ARGS__)
 #else
 #  define DEBUG_TRACE(...)
+#  define DEBUG_WARNING(...)
 #endif
