8c8e76aa43957e86b370358c5ddffc8f55e1aeb7
Bryan <bryanlaura2003@gmail.com>
2024-07-26 08:04:50 -0500

    se agregó el nivel 0 (pantalla de inicio),

--- src/game/level0.cc
+++ src/game/level0.cc
@@ -3,10 +3,34 @@
 #include "engine/systems/draw.h"
 #include "engine/components/text.h"
 #include "engine/components/position.h"
+#include "engine/systems/animation.h"
+#include "engine/components/animation.h"
+#include "engine/components/sprite.h"
 #include "engine/raylib.h"
+
+using namespace raylib;  
 
 namespace game {
   void Level0::onInit() {
+
+    // LA PANTALLA DE INICIO ES DE 1440 - 720   y los buttoms    256 - 80
+    inicio = engine::assets::Texture::MakeFromPath(R"(C:\Users\Usuario\Desktop\game\data\level0\inicio.png)");
+
+    textureAtlas["btn_jugar"].makeFromTexture(
+      engine::assets::Texture::MakeFromPath(R"(C:\Users\Usuario\Desktop\game\data\level0\btn-jugar.png)"),
+      {.size = {256, 80}}
+    );
+
+    textureAtlas["btn_1p"].makeFromTexture(
+      engine::assets::Texture::MakeFromPath(R"(C:\Users\Usuario\Desktop\game\data\level0\btn-1player.png)"),
+      {.size = {256, 80}}
+    );
+
+    textureAtlas["btn_2p"].makeFromTexture(
+      engine::assets::Texture::MakeFromPath(R"(C:\Users\Usuario\Desktop\game\data\level0\btn-2players.png)"),
+      {.size = {256, 80}}
+    );
+
     auto& sysMgr = systemManager();
     auto& entMgr = entityManager();
 
@@ -17,7 +41,38 @@ namespace game {
     nubes->addComponent<engine::PositionComponent>(0.f, 0.f);
   }
 
+void Level0::initializeEntityManager() {
+    auto& entMgr = entityManager();
+
+    btn_jugar = entMgr.addEntity();
+    btn_jugar->addComponent<engine::PositionComponent>(530.f,490.f);
+    btn_jugar->addComponent<engine::SpriteComponent>("", textureAtlas["btn_jugar"]);
+    btn_jugar->addComponent<engine::AnimationComponent>("", 0, 3, 1);
+
+    btn_1p = entMgr.addEntity();
+    btn_1p->addComponent<engine::PositionComponent>(306.f,490.f);
+    btn_1p->addComponent<engine::SpriteComponent>("", textureAtlas["btn_1p"]);
+    btn_1p->addComponent<engine::AnimationComponent>("", 0, 3, 1);
+
+    btn_2p = entMgr.addEntity();
+    btn_2p->addComponent<engine::PositionComponent>(754.f,490.f);
+    btn_2p->addComponent<engine::SpriteComponent>("", textureAtlas["btn_2p"]);
+    btn_2p->addComponent<engine::AnimationComponent>("", 0, 3, 1);
+
+    // nubesAnimate = entMgr.addEntity();
+    // nubesAnimate->addComponent<engine::PositionComponent>(0.f,0.f);
+    // nubesAnimate->addComponent<engine::SpriteComponent>("", "nubes");
+    // nubesAnimate->addComponent<engine::AnimationComponent>("", 0, 3, 0.5);
+    
+  }
+
   void Level0::onUpdate(float deltatime) {
 
+  }
+
+  void Level0::onRender() {
+    using namespace raylib;
+    rlScalef(1.f, 1.f, 1.f);
+    DrawTexture(inicio, 0, 0, WHITE);
   }
 } // namespace game
--- src/game/level0.h
+++ src/game/level0.h
@@ -1,11 +1,28 @@
 #pragma once
 #include "engine/level.h"
+#include "engine/assets/texture.h"
+#include "engine/assets/texture_atlas.h"
+
+#include <unordered_map>
 
 namespace game {
   // Selección
   class Level0 : public engine::Level {
-  public:
+    public:
+    std::unordered_map<const char*, engine::assets::TextureAtlas> textureAtlas;
+
+    engine::assets::Texture inicio;
+
+    engine::EntityPtr btn_jugar;
+    engine::EntityPtr btn_1p;
+    engine::EntityPtr btn_2p;
+
+    void initializeSystemManager();
+    void initializeEntityManager();
+
     virtual void onInit() override;
+    virtual void onProcessInput() override;
     virtual void onUpdate(float deltatime) override;
+    virtual void onRender() override;
   };
 } // namespace game
