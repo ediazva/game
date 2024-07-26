f231bf6c8d68d8b2605236892555c7e075a332e0
Arthur-P-04 <localpat1@gmail.com>
2024-07-25 22:19:15 -0500

    Update hitbox.h

--- src/engine/components/hitbox.h
+++ src/engine/components/hitbox.h
@@ -1,18 +1,28 @@
 #pragma once
 #include "engine/components/component.h"
 #include "raylib.h"
+#include <functional> 
 
 namespace engine {
   struct HitboxComponent : public Component {
-    // Hitbox circular por el momento
-    float radius{};
+    float width;
+    float height;  // tamaño del cuadrado 
     // TODO: Cambiar hitbox rectangular
     // TODO: Añadir OnCollision, OnCollisionEnter
 
-    bool alive{ true }; // Si esta muerto no es interactuable
+    bool alive{ true }; // Si está muerto no es interactuable
+
+    std::function<void()> onCollision;
+    std::function<void()> onCollisionEnter;
+    std::function<void()> onCollisionExit;
 
     HitboxComponent() = default;
-    HitboxComponent(float radius) : radius(radius) {
-    }
+    // Constructor para poner el tamaño
+    HitboxComponent(float w, float h) : width(w), height(h) {}
+
+    // Callbacks
+    void setOnCollision(std::function<void()> callback) { onCollision = callback; }
+    void setOnCollisionEnter(std::function<void()> callback) { onCollisionEnter = callback; }
+    void setOnCollisionExit(std::function<void()> callback) { onCollisionExit = callback; }
   };
 } // namespace engine
